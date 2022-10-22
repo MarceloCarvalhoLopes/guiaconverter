e2789fcd351ad5616ae6b103b1780ae60b061edc
MarceloCarvalhoLopes <marcelolcarvalho@gmail.com>
2022-10-22 09:43:59 -0300

    virtual field

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const Reader = require("./Reader");
 const Processor = require("./Processor");
-
+const Table = require("./Table");
 
 var leitor = new Reader();
 
@@ -9,6 +9,14 @@ async function main(){
 
     var dados = await leitor.Read("./users.csv");
     var dadosProcessados = Processor.Process(dados);
+
+    var usuarios = new Table(dadosProcessados);
+
+    /*usuarios.rows.push(["João joão", "C#","50"]);*/
+
+    console.log(usuarios.RowCount);
+    console.log(usuarios.ColumnCount);
+
 }
 
 main();
