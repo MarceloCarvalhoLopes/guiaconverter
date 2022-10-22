055ea3077429aa54f6a85122fc11c26c8fcecca1
MarceloCarvalhoLopes <marcelolcarvalho@gmail.com>
2022-10-22 15:17:38 -0300

    PDFWriter

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const Writer = require("./Writer");
 const Processor = require("./Processor");
 const Table = require("./Table");
 const HtmlParser = require("./HtmlParser");
+const PDFWriter = require("./PDFWriter");
+
 
 var leitor = new Reader();
 var escritor = new Writer();
@@ -16,6 +18,8 @@ async function main(){
     var html = await HtmlParser.Parse(usuarios);
 
     escritor.Write(Date.now() + ".html", html);
+
+    PDFWriter.WritePdf(Date.now() + ".PDF", html);
 
 
 }
